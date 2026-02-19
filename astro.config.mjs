504cc678acc684eba1527ebdac09e08d9fa799fe
emrekayik <emrekayik1905@gmail.com>
2026-02-19 16:56:32 +0300

    first version

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,24 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+
+import tailwindcss from "@tailwindcss/vite";
+
+import react from "@astrojs/react";
+
+import svelte from "@astrojs/svelte";
+
+import alpinejs from "@astrojs/alpinejs";
+
+import vue from "@astrojs/vue";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://studioek.github.io',
+  vite: {
+    plugins: [tailwindcss()]
+  },
+
+  integrations: [react(), svelte(), alpinejs(), vue(), sitemap()]
+});
